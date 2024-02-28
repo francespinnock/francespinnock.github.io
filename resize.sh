a577b602977692fc162bd32fd04514511b66e636
Harry Askham <harryaskham@gmail.com>
2024-02-28 20:59:42 +0000

    [no commit message]

--- resize.sh
+++ resize.sh
@@ -1,4 +1,10 @@
 for f in _projects/*; do
   echo $f
-  sed -i "" "s/png/jpg/g" "$f"
+  JPGS=$(cat $f | grep -o "\".*\.jpg.*\"" | xargs -I% echo ".%")
+  for jpg in $JPGS; do
+    png=$(echo $jpg | sed "s/jpg/png/")
+    echo "converting '$jpg' to '$png'"
+    magick "$jpg" -resize 50% "$png"
+  done
+  sed -i "s/jpg/png/g" "$f"
 done
